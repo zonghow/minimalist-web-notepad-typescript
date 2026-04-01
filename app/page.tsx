dfd874146f2aa7dced986c02853a5460a15fb7b6
zonghow <zonghao1995@gmail.com>
2026-04-01 19:26:39 +0800

    feat(page.tsx): add dynamic export to force dynamic rendering on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { generateRandomNoteName } from "@/lib/note-name";
 
+export const dynamic = "force-dynamic";
+
 export default function HomePage() {
   redirect(`/${generateRandomNoteName()}`);
 }
